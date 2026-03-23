197a97f5d81cd4e1f23358a9dbc42cab1259d04a
Manoj Patil M <smmanoj208@gmail.com>
2026-03-23 22:03:09 +0530

    updated image
    updated image

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import HeroImg from '../../images/1721275616026.jpg'
+import HeroImg from '../../images/1772337632327.jpg'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
